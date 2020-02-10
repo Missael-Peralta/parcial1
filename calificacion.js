e5b00fc3d80a37dc78ac67c6048eee00ff4c69bc
Missael-Peralta <60717142+Missael-Peralta@users.noreply.github.com>
2020-02-10 16:32:47 -0600

    Add files via upload
    calificacion

---
+++ calificacion.js
@@ -0,0 +1,11 @@
+var x=Math.floor(Math.random()*10)+1;
+console.log(x);
+if(x<6)
+    console.log("La calificacion es F");
+else
+    if(x==9)
+        console.log("La calificacion es B");
+    else
+        if(x==10)
+            console.log("La calificacion es A");
+
