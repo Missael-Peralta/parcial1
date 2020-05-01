8f4be3284717ca040548d494bac5d0653145ea3e
Missael-Peralta <60717142+Missael-Peralta@users.noreply.github.com>
2020-05-01 00:04:19 -0500

    Add files via upload
    Las clases

---
+++ figuras.js
@@ -0,0 +1,30 @@
+class Rectangulo{
+    constructor(base,altura){
+        this.base=base;
+        this.altura=altura;
+    }
+    area(){
+        return this.base*this.altura;
+    }
+}
+ 
+class Triangulo{
+    constructor(base,altura){
+        this.base=base;
+        this.altura=altura;
+    }
+    area(){
+        return this.base*this.altura/2;
+    }
+}
+ 
+class Cuadrado{
+    constructor(base,altura){
+        this.base=base;
+        this.altura=altura;
+    }
+    area(){
+        return this.base*this.altura;
+    }
+}
+ 
